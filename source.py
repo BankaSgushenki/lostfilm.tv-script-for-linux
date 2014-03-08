45fae21f172be115b9cd7d232edf18b8d1b6e0c8
Kostya Sergeev <kostyasergeev93@gmail.com>
2014-03-08 23:34:24 +0200

    fix

--- source.py
+++ source.py
@@ -3,17 +3,20 @@
 
 import subprocess
 import urllib
+import os
+
+ROOT_PATH=os.path.dirname(__file__) + "/lost.txt"
 
 def sendmessage(message):
-    subprocess.Popen(['notify-send', message])
-    return
+	subprocess.Popen(['notify-send', message])
+	return
 
 def load_html():
 	url = 'http://www.lostfilm.tv'
-	return urllib.urlopen(url) 
+	return urllib.urlopen(url)
 
 infile = load_html()
-file = open("/home/kostya/bin/lost.txt", "r")
+file = open(ROOT_PATH, "r")
 temp = file.readline()
 file.close()
 lines = infile.readlines()
@@ -21,10 +24,10 @@ for i in range(len(lines)):
 	line = lines[i]
 	if "text-decoration:none" in line:
 		serial_name = lines[i][170:-13]	
-		serial_name = serial_name + "  (new episod on lostfilm.tv)"			
+		serial_name = serial_name + " (new episod on lostfilm.tv)"	
 		break
 if temp != serial_name:
 	sendmessage(unicode(serial_name, 'cp1251'))
-file = open("/home/kostya/bin/lost.txt", "w")
+file = open(ROOT_PATH, "w")
 file.write(serial_name)
 file.close()
