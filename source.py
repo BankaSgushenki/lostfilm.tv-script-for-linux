3db79d0bca7ff3390ce9a185ff0610d3816cc3fe
BankaSgushenki <kostyasergeev93@gmail.com>
2014-03-08 21:45:48 +0300

    update

--- source.py
+++ source.py
@@ -1,6 +1,13 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import subprocess
+
 import urllib
+
+def sendmessage(message):
+    subprocess.Popen(['notify-send', message])
+    return
 
 def load_html():
 	url = 'http://www.lostfilm.tv'
@@ -8,12 +15,24 @@ def load_html():
 
 infile = load_html()
 
+file = open("/home/kostya/Development/Python/lostfilmScript/lostfilm.tv-app/lost.txt", "r")
+temp = file.readline()
+file.close()
+
 lines = infile.readlines()
 for i in range(len(lines)):
 	line = lines[i]
 	if "text-decoration:none" in line:
-		serial_name = lines[i][170:-13]		
-		print unicode(serial_name, 'cp1251')
+		serial_name = lines[i][170:-13]	
+		serial_name = serial_name + "  (new episod on lostfilm.tv)"			
 		episod_name = lines[i+3][166:-13]
-		print unicode(episod_name, 'cp1251')
-	
+		break
+
+if temp == serial_name:
+	sendmessage(unicode(serial_name, 'cp1251'))
+
+file = open("/home/kostya/Development/Python/lostfilmScript/lostfilm.tv-app/lost.txt", "w")
+file.write(serial_name)
+file.close()
+		
+
